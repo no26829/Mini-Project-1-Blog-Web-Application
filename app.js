862d09732b6dd6b88abffbf5d8f3dbc6b8783f5c
no26829 <102497309+no26829@users.noreply.github.com>
2024-09-16 19:35:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+// Sets the port to 3000 locally
+const PORT = process.env.PORT || 3000;
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Set the correct path for the views dir
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up routes
+const indexRoutes = require('./src/routes/index');
+app.use('/', indexRoutes);
+
+// Start the server locally
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
